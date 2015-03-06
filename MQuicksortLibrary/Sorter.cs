e0a08807f1e09bcb438d23dffc69221519cb56de
RybinVladislav <rybin@me.com>
2015-03-06 15:42:02 +0300

    initial commit

---
+++ MQuicksortLibrary/Sorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace MQuicksortLibrary
+{
+	public class SortException : ApplicationException
+	{		
+		public SortException(string message) : base(message) {}
+	}
+	public static class Sorter
+	{
+		public delegate int CompareDelegate<T>(T a, T b);
+		public static void QuickSort<T>(this List<T> list, CompareDelegate<T> comparer)
+		{
+			if (comparer == null)
+				throw new SortException("No comparer provided");
+			int len = list.Count;
+			list.QuickSortRecursive(0,len-1, comparer);
+		}
+		private static void QuickSortRecursive<T>(this List<T> list, int left, int right, CompareDelegate<T> comparer)
+		{
+			if (left < right)
+				new Thread (list.Partition(left,right,comparer)).Start();
+		}
+		private static void Partition<T>(this List<T> list, int left, int right, CompareDelegate<T> comparer)
+		{
+			Console.WriteLine(default(int));
+		}
+	}
+}
+
---
+++ MultithreadingQuicksort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MQuicksortLibrary;
+
+
+namespace MultithreadingQuicksort
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			List<int> array = new List<int> ();
+			Random rnd = new Random ();
+			for (int i =0; i<20;i++)
+				array.Add(rnd.Next(0,100));
+			array.QuickSort ();
+			foreach (int elem in array)
+				Console.Write ("{0} ",elem);
+		}
+	}
+}
