ca1f32ecfc35e9842d00e3d8493d5b85fb3656d2
RybinVladislav <rybin@me.com>
2015-03-07 22:34:18 +0300

    working sort

--- MQuicksortLibrary/Sorter.cs
+++ MQuicksortLibrary/Sorter.cs
@@ -4,28 +4,66 @@ using System.Collections.Generic;
 
 namespace MQuicksortLibrary
 {
-	public class SortException : ApplicationException
+	class SortException : ApplicationException
 	{		
 		public SortException(string message) : base(message) {}
 	}
+
 	public static class Sorter
 	{
 		public delegate int CompareDelegate<T>(T a, T b);
+		public static Thread QuickSortThread<T>(this List<T> list, int a, int b, CompareDelegate<T> comparer) {
+			var t = new Thread(() => list.QuickSortRecursive(a, b,comparer));
+			t.Start();
+			return t;
+		}
 		public static void QuickSort<T>(this List<T> list, CompareDelegate<T> comparer)
 		{
 			if (comparer == null)
 				throw new SortException("No comparer provided");
 			int len = list.Count;
-			list.QuickSortRecursive(0,len-1, comparer);
+			list.QuickSortThread(0,len-1, comparer);
+
+		}
+		private static void Swap<T>(T a, T b)
+		{
+			T tmp = a;
+			a = b;
+			b = tmp;
 		}
 		private static void QuickSortRecursive<T>(this List<T> list, int left, int right, CompareDelegate<T> comparer)
 		{
-			if (left < right)
-				new Thread (list.Partition(left,right,comparer)).Start();
-		}
-		private static void Partition<T>(this List<T> list, int left, int right, CompareDelegate<T> comparer)
-		{
-			Console.WriteLine(default(int));
+			T pivot=list[(left+right)/2];
+			int a = left, b = right;
+			while (a <= b)
+			{
+				while (comparer(list[a],pivot)<0) //list[a] *more* than pivot
+				{
+					a++;
+				}
+				while (comparer(list[b],pivot)>0) //list[b] *more* than pivot
+				{
+					b--;
+				}
+				if (a <= b)
+				{
+					// Swap
+					T tmp = list[a];
+					list[a] = list[b];
+					list[b] = tmp;
+
+					a++;
+					b--;
+				}
+			}
+			if (left < b)
+			{
+				QuickSortRecursive (list, left, b, comparer); 
+			}
+			if (a < right)
+			{
+				QuickSortRecursive (list, a, right, comparer);
+			}
 		}
 	}
 }
--- MultithreadingQuicksort/Program.cs
+++ MultithreadingQuicksort/Program.cs
@@ -7,15 +7,24 @@ namespace MultithreadingQuicksort
 {
 	class MainClass
 	{
+		public static int MyCompare(int a, int b)
+		{
+			if (a<b)
+				return -1;
+			if (a > b)
+				return 1;
+			else
+				return 0;
+		}
 		public static void Main (string[] args)
 		{
 			List<int> array = new List<int> ();
 			Random rnd = new Random ();
 			for (int i =0; i<20;i++)
 				array.Add(rnd.Next(0,100));
-			array.QuickSort ();
-			foreach (int elem in array)
-				Console.Write ("{0} ",elem);
+			array.QuickSort (MyCompare);
+			for(int i =0; i<array.Count;i++)
+				Console.Write ("{0} ",array[i]);
 		}
 	}
 }
